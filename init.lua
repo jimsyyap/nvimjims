c1e8e6212a295906bf4adac8a11a07fb1dbb1123
jimmy github <jimsyyap@gmail.com>
2025-12-20 08:37:54 +1100

    autocap is joy

--- init.lua
+++ init.lua
@@ -2,3 +2,19 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 require("config.lazy")
+
+for i = string.byte("a"), string.byte("z") do
+  local lower = string.char(i)
+  local upper = string.char(i - 32)
+  vim.keymap.set("i", lower, function()
+    return vim.fn.search([[\v[.!?]\s+%#]], "bcnw") ~= 0 and upper or lower
+  end, { expr = true })
+end
+
+for i = string.byte("A"), string.byte("Z") do
+  local upper = string.char(i)
+  local lower = string.char(i + 32)
+  vim.keymap.set("i", upper, function()
+    return vim.fn.search([[\v[.!?]\s+%#]], "bcnw") ~= 0 and lower or upper
+  end, { expr = true })
+end
